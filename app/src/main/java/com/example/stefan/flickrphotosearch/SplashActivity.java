7bd88cc782ec516f5858332e929441e4a60ea33e
Stefan Kochev <stefan_kochev@yahoo.com>
2017-08-03 20:25:20 +0200

    SplashScreen work!
    SplashScreen implementation changed from Threads work to Handler

--- app/src/main/java/com/example/stefan/flickrphotosearch/SplashActivity.java
+++ app/src/main/java/com/example/stefan/flickrphotosearch/SplashActivity.java
@@ -2,35 +2,32 @@ package com.example.stefan.flickrphotosearch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class HomeActivity extends Activity {
+import com.example.stefan.flickrphotosearch.model.GalleryItem;
+
+public class SplashActivity extends Activity {
+
+    private static final int KEEP_TIME = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_splash);
 
-        Thread myThread = new Thread(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    sleep(3000);
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                    finish();
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
             }
-        };
-        myThread.start();
+        }, KEEP_TIME);
     }
 }
